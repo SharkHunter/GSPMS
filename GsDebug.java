15b707d79bc9174f0676dbe46345aa12161f34ae
SharkHunter <bajen.hasse@gmail.com>
2011-01-30 12:29:51 +0100

    Revert

--- GsDebug.java
+++
@@ -1,6 +0,0 @@
-package net.pms.external;
-
-public interface GsDebug {
-	void debug(String str,int level);
-	void error(String str);
-}
--- SearchObj.java
+++
@@ -1,6 +0,0 @@
-package net.pms.external;
-import net.pms.dlna.*;
-
-public interface SearchObj {
-	void search(String searchString,DLNAResource searcher);
-}
